ecdb90f3780fb68cfc4abee2c41b4857339385f3
Kevin <devkevinsoria@gmail.com>
2022-07-12 15:11:29 -0300

    conexión a api c/ jason-server. Variante de color en formulario

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -1,26 +1,86 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const Crud = () => {
-   const [db, setDb] = useState([]);
+   const [db, setDb] = useState(null);
    const [dataToEdit, setDataToEdit] = useState(null);
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+
+   let api = helpHttp();
+   let url = "http://localhost:5000/users";
+
+   useEffect(() => {
+      setLoading(true);
+      api.get(url).then((res) => {
+         if (!res.err) {
+            setDb(res);
+            setError(null);
+         } else {
+            setDb(null);
+            setError(res);
+         }
+      });
+      setLoading(false);
+   }, []);
 
    const createData = (data) => {
       data.id = db.length + 1;
-      setDb([...db, data]);
+      // Solo para Json-server, cada API necesita sus options.
+      let options = {
+         body: data,
+         headers: { "content-type": "application/json" },
+      };
+
+      api.post(url, options).then((res) => {
+         console.log(res);
+
+         if (!res.err) {
+            setDb([...db, res]);
+         } else {
+            setError(res);
+         }
+      });
    };
 
    const updateData = (data) => {
-      let newData = db.map((el) => (el.id === data.id ? data : el));
-      setDb(newData);
+      // Solo para Json-server, cada API necesita sus options.
+      let options = {
+         body: data,
+         headers: { "content-type": "application/json" },
+      };
+      let endpoint = `${url}/${data.id}`;
+
+      api.put(endpoint, options).then((res) => {
+         if (!res.err) {
+            let newData = db.map((el) => (el.id === data.id ? data : el));
+            setDb(newData);
+         } else {
+            setError(res);
+         }
+      });
    };
 
    const deleteData = (id) => {
       let isDelete = confirm(`¿Desea eliminar el registro ${id}?`);
       if (isDelete) {
-         let filterData = db.filter((el) => el.id !== id);
-         setDb(filterData);
+         let endpoint = `${url}/${id}`;
+         let options = {
+            headers: { "content-type": "application/json" },
+         };
+
+         api.del(endpoint, options).then((res) => {
+            if (!res.err) {
+               let filterData = db.filter((el) => el.id !== id);
+               setDb(filterData);
+            } else {
+               setError(res);
+            }
+         });
       } else {
          return;
       }
@@ -35,12 +95,16 @@ const Crud = () => {
             setDataToEdit={setDataToEdit}
             className="w-full md:w-2/5"
          />
+         {loading && <Loader />}
+         {error && <Message />}
          <div className="w-4/5 lg:w-3/5 portrait:hidden md:portrait:block">
-            <CrudTable
-               data={db}
-               setDataToEdit={setDataToEdit}
-               deleteData={deleteData}
-            />
+            {db && (
+               <CrudTable
+                  data={db}
+                  setDataToEdit={setDataToEdit}
+                  deleteData={deleteData}
+               />
+            )}
          </div>
          <div className="md:portrait:hidden sm:landscape:hidden w-3/4 text-center border border-blue-600">
             <h1 className="font-bold text-2xl p-4 text-blue-700 dark:text-gray-400">
--- src/components/CrudTable.jsx
+++ src/components/CrudTable.jsx
@@ -3,7 +3,7 @@ import CrudTableRow from "./CrudTableRow";
 const CrudTable = ({ data, setDataToEdit, deleteData }) => {
    return (
       <div className="relative overflow-x-visible shadow-md sm:rounded-lg">
-         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-300">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700/50 dark:text-gray-400">
                <tr>
                   <th scope="col" className="px-6 py-3">
@@ -21,16 +21,7 @@ const CrudTable = ({ data, setDataToEdit, deleteData }) => {
                </tr>
             </thead>
             <tbody>
-               {data.length === 0 ? (
-                  <tr className="bg-gray-700/20 border-b dark:border-blue-700/70">
-                     <td
-                        colSpan="4"
-                        className="text-xl font-medium text-bold text-center p-3"
-                     >
-                        Sin datos
-                     </td>
-                  </tr>
-               ) : (
+               {data.length > 0 ? (
                   data.map((el) => (
                      <CrudTableRow
                         key={el.id}
@@ -39,6 +30,15 @@ const CrudTable = ({ data, setDataToEdit, deleteData }) => {
                         deleteData={deleteData}
                      />
                   ))
+               ) : (
+                  <tr className="bg-gray-700/20 border-b dark:border-blue-700/70">
+                     <td
+                        colSpan="4"
+                        className="text-xl font-medium text-bold text-center p-3"
+                     >
+                        Sin datos
+                     </td>
+                  </tr>
                )}
             </tbody>
          </table>
